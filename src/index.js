f5eb8883584df5748f6dd65be4a22bc8e49b0ff6
Capi Etheriel <barraponto@gmail.com>
2017-03-27 15:30:48 -0300

    style(eslint): use neutrino-dev eslintrc

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const merge = require('deepmerge');
 
 module.exports = (neutrino, options) => {
   neutrino.use(lint, merge({
-      eslint: {
-        baseConfig: {
-          extends: ['eslint:recommended', 'google'],
-        },
-      },
+    eslint: {
+      baseConfig: {
+        extends: ['eslint:recommended', 'google']
+      }
+    }
   }, options));
 
   if (!options.include && !options.exclude) {
