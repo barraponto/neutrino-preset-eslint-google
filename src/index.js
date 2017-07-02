aaebc63d29221f9ea42b3bd167bd34895db30465
Capi Etheriel <barraponto@gmail.com>
2017-07-02 04:09:03 -0300

    refactor(preset): set options.include and options.exclude like neutrino-middleware-airbnb-base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const lint = require('neutrino-middleware-eslint');
 const merge = require('deepmerge');
 
-module.exports = (neutrino, options) => {
-  neutrino.use(lint, merge({
-    eslint: {
-      baseConfig: {
-        extends: ['eslint:recommended', 'google']
+module.exports = (neutrino, options = {}) => {
+  neutrino.use(lint, merge.all([
+    {
+      eslint: {
+        baseConfig: {
+          extends: ['eslint:recommended', 'google']
+        }
       }
-    }
-  }, options));
-
-  if (!options.include && !options.exclude) {
-    neutrino.config.module.rule('lint')
-      .include.add(neutrino.options.source);
-  }
+    },
+    options,
+    (!options.include && !options.exclude) ?
+      { include: [neutrino.options.source], exclude: [neutrino.options.static] } :
+      {}
+  ]));
 };
